31b89ac2a77fd1011c24871cbdd9767dfcff49a8
Bernardo Maggessi <bermaggessi2@yahoo.com>
2022-01-03 14:16:30 -0300

    piece moce count

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -6,14 +6,24 @@ import boardgame.Position;
 public abstract class ChessPiece extends Piece {
 
 	private Color color;
+	private int moveCount;
 
 	public ChessPiece(Board board, Color color) {
 		super(board);
 		this.color = color;
 	}
+	public void increaseMoveCount() {
+		moveCount++;
+	}
+	public void decreaseMoveCount() {
+		moveCount--;
+	}
 
 	public Color getColor() {
 		return color;
+	}
+	public int getMoveCount() {
+		return moveCount;
 	}
 	public ChessPosition getChessPosition() {
 		return ChessPosition.fromPosition(position);
@@ -23,5 +33,6 @@ public abstract class ChessPiece extends Piece {
 		ChessPiece p = (ChessPiece)getBoard().piece(position);
 		return p != null && p.getColor() != color;
 	}
+	
 	
 }
